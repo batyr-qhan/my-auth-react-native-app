89331e8b565f6d4037fd5c4518cf01d5afe3371c
batyrqh <batyrqh@protonmail.com>
2022-04-27 15:57:40 +0300

    added Login screen, Signup screen, auth utils, etc

---
+++ screens/SignupScreen.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { createUser } from "../utils/auth";
+
+function SignupScreen() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  async function signupHandler({ email, password }) {
+    setIsAuthenticating(true);
+    await createUser(email, password);
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="it is being processed... " />;
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
+}
+
+export default SignupScreen;
---
+++ utils/auth.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API_KEY = "AIzaSyAAzowwB39RDqxqfTDR3N0mhl3xPQvTmZQ";
+
+export async function createUser(email, password) {
+  const response = await axios.post(
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
+    {
+      email: email,
+      password,
+      password,
+      returnSecureToken: true,
+    }
+  );
+}
